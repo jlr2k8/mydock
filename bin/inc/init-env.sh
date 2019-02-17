1029d52e854b93f5d0ce91002c0435f932134ee6
Josh Rogers <mail@joshlrogers.com>
2019-02-16 17:27:01 -0800

    make host port configurable, move export logic outside if/else, cleanup

--- bin/inc/init-env.sh
+++ bin/inc/init-env.sh
@@ -5,6 +5,7 @@ function setEnv()
     local ENV="${1}"
     local WORKING_COPY="${2}"
     local DOCKER_REPO="${3}"
+    local HOST_PORT="${4}"
 
     if [[ -z "${ENV}" ]]; then
         echo
@@ -14,14 +15,14 @@ function setEnv()
 
     echo "WORKING_COPY=${WORKING_COPY}" > ${ENV}
 
-    # Docker repo
     if [[ -z "${DOCKER_REPO}" ]]; then
         echo
-        echo "Fatal - Docker repo must be specified for setWorkingCopy()"
+        echo "Fatal - Docker repo must be specified"
         return 1
     fi
 
     echo "DOCKER_REPO=${DOCKER_REPO}" >> ${ENV}
+    echo "HOST_PORT=${HOST_PORT}" >> ${ENV}
 
     return 0
 }
@@ -47,13 +48,18 @@ function getEnv()
         echo "Please provide the docker repository name:"
         read -p "Docker Repo: " DOCKER_REPO
 
-        setEnv "${ENV}" "${WORKING_COPY}" "${DOCKER_REPO}"
+        echo
+        echo "Please provide the host port (or skip by leaving blank):"
+        read -p "Host port: " HOST_PORT
+
+        setEnv "${ENV}" "${WORKING_COPY}" "${DOCKER_REPO}" "${HOST_PORT}"
     else
         . ${ENV}
-
-        export WORKING_COPY="${WORKING_COPY}"
-        export DOCKER_REPO="${DOCKER_REPO}"
     fi
+
+    export WORKING_COPY="${WORKING_COPY}"
+    export DOCKER_REPO="${DOCKER_REPO}"
+    export HOST_PORT="${HOST_PORT}"
 
     return 0
 }
