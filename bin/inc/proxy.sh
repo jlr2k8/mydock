9e07f37be1919a8794f1efd1b88199aaf8b97d67
Josh Rogers <mail@joshlrogers.com>
2019-06-26 10:39:25 -0700

    ssl port

--- bin/inc/proxy.sh
+++ bin/inc/proxy.sh
@@ -2,6 +2,20 @@
 
 function buildRunProxy()
 {
+    if [[ -z "${HOST_PORT}" ]]; then
+        echo
+        echo 'HOST_PORT was not set, even when attempting to get the proxy set up! Defaulting to 80....'
+        echo
+        HOST_PORT=80
+    fi
+
+    if [[ -z "${HOST_PORT_SSL}" ]]; then
+        echo
+        echo 'HOST_PORT_SSL was not set, even when attempting to get the proxy set up! Defaulting to 443....'
+        echo
+        HOST_PORT_SSL=443
+    fi
+
     docker-compose \
         --file "${MYDOCK_ROOT}/utilities/proxy/docker-compose.yml" \
         up \
