cc519e4dad07520d6123d68476122c2d4afe5a8c
Josh Rogers <mail@joshlrogers.com>
2019-07-28 08:00:32 -0700

    attempted stop if project does not contain a stop.sh file

--- bin/commands/stop.sh
+++ bin/commands/stop.sh
@@ -28,4 +28,17 @@ PROJECT_DESTROY_FILE="${MYDOCK_ROOT}/projects/${PROJECT}/stop.sh"
 if [[ -x "${PROJECT_DESTROY_FILE}" ]]; then
     shift 1
     . "${PROJECT_DESTROY_FILE}" $@
+else
+    echo
+    echo "This project does not appear to have an official stop script (${PROJECT_DESTROY_FILE}. Attempting 'docker stop ${PROJECT_NAME}'"
+    echo
+    docker stop ${PROJECT_NAME}
+
+    if [[ $? != 0 ]]; then
+        echo
+        echo "Hrm.... well, here is the list of running containers. You'll have to manually run 'docker stop' against whatever containers are running for this project."
+        echo
+        docker ps
+        echo
+    fi
 fi
