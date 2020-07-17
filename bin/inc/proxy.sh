3999a2aafc1b1c819b33d64ec9281eed26cf1c31
Josh Rogers <mail@joshlrogers.com>
2020-07-16 19:18:05 -0700

    verbiage

--- bin/inc/proxy.sh
+++ bin/inc/proxy.sh
@@ -4,21 +4,21 @@ function buildRunProxy()
 {
     if [[ -z "${HOST_PORT}" ]]; then
         echo
-        echo 'HOST_PORT was not set, even when attempting to get the proxy set up! Defaulting to 80....'
+        echo 'Defaulting host port to 80....'
         echo
         HOST_PORT=80
     fi
 
     if [[ -z "${HOST_PORT_SSL}" ]]; then
         echo
-        echo 'HOST_PORT_SSL was not set, even when attempting to get the proxy set up! Defaulting to 443....'
+        echo 'Defaulting host SSL port to 443....'
         echo
         HOST_PORT_SSL=443
     fi
 
     if [[ -z "${HOST_CERT_DIR}" ]]; then
         echo
-        echo 'HOST_CERT_DIR was not set, even when attempting to get the proxy set up! Defaulting to "/usr/local/apache2/ssl"....'
+        echo 'Defaulting cert/key dir to "/usr/local/apache2/ssl"....'
         echo
         HOST_CERT_DIR='/usr/local/apache2/ssl'
     fi
