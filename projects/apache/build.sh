e425a6f41c119adad6c54b1fe7253bb69f766b78
josh <mail@joshlrogers.com>
2024-12-27 19:39:02 -0800

    rm exception for apache 2.4.52 - 2.4.57

--- projects/apache/build.sh
+++ projects/apache/build.sh
@@ -8,13 +8,7 @@ if [[ -z "${DOCKER_REPO}" ]]; then
     echo "DOCKER_REPO=${DOCKER_REPO}" >> ${ENV}
 fi
 
-# NOTE: Apache 2.4.57 (latest as of 2023-08-13) has an issue with PCRE - I even tried upgrading to PCRE3, also tried
-# using the OS build instead of compiling... etc. Nothing worked. Somehow between Apache 2.4.52 - 2.4.57, something
-# weird is happening. For now, I'm stubbing out the curl command that pulls the latest and hard-coding in the last known
-# version number that works.
-
-#APACHE_VERSION=$(curl -s 'https://downloads.apache.org//httpd/CHANGES_2.4' | head -2 | tail -1 | awk {'print $4'})
-APACHE_VERSION="2.4.52"
+APACHE_VERSION=$(curl -s 'https://downloads.apache.org//httpd/CHANGES_2.4' | head -2 | tail -1 | awk {'print $4'})
 
 echo "(sorta) Latest Apache 2.4: ${APACHE_VERSION}"
 echo
